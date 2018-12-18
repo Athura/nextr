a07fcf512f22c030ecef727e0dde7ead71cd88f5
Athura <joshualjohnson33@gmail.com>
2018-12-18 08:11:43 -0500

    Begin App Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { PageWrapper } from './styles/common/page-wrapper';
 import { Wrapper } from './styles/common/wrapper';
 
 class App extends Component {
   render() {
     return (
-      <PageWrapper>
-        <Wrapper>
-        </Wrapper>
-      </PageWrapper>
+      <Router>
+        <PageWrapper>
+          <Wrapper>
+            <Route exact path = "/" component={} />
+          </Wrapper>
+        </PageWrapper>
+      </Router>
     );
   }
 }
---
+++ frontend/src/components/AppBar/AppBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/Badge';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/core/'
+
+class PrimaryAppBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PrimaryAppBar;
