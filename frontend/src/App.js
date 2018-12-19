3ade9e82818bc797918b03f7fa82244faece9b7b
Athura <joshualjohnson33@gmail.com>
2018-12-19 08:13:03 -0500

    Begin Appbar / Login / Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { PageWrapper } from './styles/common/page-wrapper';
-import { Wrapper } from './styles/common/wrapper';
+import PrimaryAppBar from './components/AppBar/AppBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <PageWrapper>
-          <Wrapper>
-            <Route exact path = "/" component={} />
-          </Wrapper>
-        </PageWrapper>
+
+          <PrimaryAppBar />
+
       </Router>
     );
   }
