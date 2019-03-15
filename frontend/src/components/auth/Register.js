3f680260561eb3dc828d078a5ca75fc0b6b59c79
Athura <joshualjohnson33@gmail.com>
2019-03-15 08:28:48 -0400

    Figured out error handling with forms
    - Added errors to forms
    - Implemented error reducer to handle errors instead of making registeruser handle it
    - TODO: CSS for errors. Make them red

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import Input from '../Form/Input';
 import { Button } from '../../styles/common/button';
-import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions'
@@ -12,14 +11,20 @@ class Register extends Component {
   constructor() {
     super();
     this.state = {
-      newUser: {
         firstname: '',
         lastname: '',
         email: '',
         password: '',
         password2: '',
         errors: {}
-      }
+    }
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
     }
   }
 
@@ -42,58 +47,69 @@ handleInput = (e) => {
     const newUser = this.state.newUser;
 
     this.props.registerUser(newUser);
-
-    // axios.post('/api/users/register', newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }))
   }
 
   render() {
 
     const { errors } = this.state;
-    const { user } = this.props.auth;
 
     return (
       <div>
-        {user ? user.name : null}
         <h1>Register</h1>
         <div className="container" onSubmit={this.handleFormSubmit}>
-          <Input
-            type={'text'}
-            title={'Firstname'}
-            name={'firstname'}
-            value={this.state.newUser.firstname}
-            placeholder={'Enter your first name here'}
-            handleChange={this.handleInput}
-          />
+          <div>
+              <Input
+                type={'text'}
+                title={'Firstname'}
+                name={'firstname'}
+                value={this.state.firstname}
+                placeholder={'Enter your first name here'}
+                handleChange={this.handleInput}
+              />
+              {errors.firstname && (
+                <div>{errors.firstname}</div>
+              )}
+          </div>
           <Input type={'text'}
             title={'lastname'}
             name={'lastname'}
-            value={this.state.newUser.lastname}
+            value={this.state.lastname}
             placeholder={'Enter your last name here'}
             handleChange={this.handleInput}
           />
+          {errors.firstname && (
+                <div>{errors.firstname}</div>
+          )}
           <Input type={'text'}
             title={'email'}
             name={'email'}
-            value={this.state.newUser.email}
+            value={this.state.email}
             placeholder={'Enter your email here'}
             handleChange={this.handleInput}
           />
+          {errors.firstname && (
+                <div>{errors.firstname}</div>
+          )}
           <Input type={'password'}
             title={'Password'}
             name={'password'}
-            value={this.state.newUser.password}
+            value={this.state.password}
             placeholder={'Your Password'}
             handleChange={this.handleInput}
           />
+          {errors.firstname && (
+                <div>{errors.firstname}</div>
+          )}
           <Input type={'password'}
             title={'Password Confirmation'}
             name={'password2'}
-            value={this.state.newUser.password2}
+            value={this.state.password2}
             placeholder={'Confirm Your Password'}
             handleChange={this.handleInput}
           />
+          {errors.firstname && (
+                <div>{errors.firstname}</div>
+          )}
           <Button register border onClick={this.handleFormSubmit}>Submit</Button>
           <Button register border>Forgot your login?</Button>
         </div>
@@ -104,11 +120,13 @@ handleInput = (e) => {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(mapStateToProps, { registerUser })(Register);
